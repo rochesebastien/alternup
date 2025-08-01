a52559b6dc21dfd99b3c4a817c7ba9c4508fea85
Roche Sébastien <sebastienroche230@gmail.com>
2025-08-02 01:15:44 +0200

    chore(deps): update to install nuxt ui 3

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,7 +1,7 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/ui'],
   typescript: {
     strict: true
   },
